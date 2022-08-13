b381bff2fbea15a49ec80814936ac4e878898ed8
kesel <a.kiselyoff@gmail.com>
2022-08-14 02:35:36 +0300

    add simple validation Joi

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
+  // const message = status === 500 ? 'Server error' : err.message;
   res.status(500).json({ message: err.message });
 });
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
 
 const {
   listContacts,
@@ -8,6 +9,12 @@ const {
   addContact,
   updateContact,
 } = require('../../models/contacts');
+
+const contactsSchema = Joi.object({
+  name: Joi.string().required(),
+  phone: Joi.string().required(),
+  email: Joi.string().required(),
+});
 
 router.get('/', async (req, res, next) => {
   try {
@@ -31,8 +38,9 @@ router.get('/:contactId', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
-    const body = req.body;
-    const newContact = await addContact(body);
+    const { error } = contactsSchema.validate(req.body);
+    if (error) res.status(400).json({ message: error.message });
+    const newContact = await addContact(req.body);
     res.status(201).json(newContact);
   } catch (error) {
     res.status(500).json({ message: 'Server error' });
@@ -44,30 +52,31 @@ router.delete('/:contactId', async (req, res, next) => {
     const { contactId } = req.params;
     const contact = await removeContact(contactId);
     console.log(contact);
-    if (!contact) {
-      res.status(404).json({ message: 'not found' });
-      return;
-    }
-    res.status(200).json({ message: 'Contact was removed', contact });
+    if (!contact) res.status(404).json({ message: 'not found' });
+    res.status(200).json({ message: 'Contact was removed', contact }); // status 200 - because 204 do not return a message
   } catch (error) {
     res.status(500).json({ message: 'Server error' });
   }
 });
 
 router.put('/:contactId', async (req, res, next) => {
-  const { contactId } = req.params;
-  const body = req.body;
-  console.log(body);
-  if (!body) {
-    res.status(400).json({ message: 'missing fields' });
-    return;
+  try {
+    const { contactId } = req.params;
+    const body = req.body;
+    console.log(body);
+    if (!body) {
+      res.status(400).json({ message: 'missing fields' });
+      return;
+    }
+    const contact = await updateContact(contactId, body);
+    if (!contact) {
+      res.status(404).json({ message: 'not found' });
+      return;
+    }
+    res.status(200).json({ message: 'Contact was updated to:', contact });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
   }
-  const contact = await updateContact(contactId, body);
-  if (!contact) {
-    res.status(404).json({ message: 'not found' });
-    return;
-  }
-  res.status(200).json({ message: 'Contact was updated to:', contact });
 });
 
 module.exports = router;
