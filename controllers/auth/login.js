f901725d32ae6115db9c9ac781a74171a3d31785
kesel <a.kiselyoff@gmail.com>
2022-08-31 18:57:30 +0300

    add token

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,13 +1,25 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const { User } = require('../../models/user');
 const { RequestError } = require('../../helpers');
+
+const { SECRET_KEY } = process.env;
 
 const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
-  if (!user) throw RequestError(401, 'Email or password is wrong');
+  if (!user) throw RequestError(401, 'Email not found');
   const comparePassword = await bcrypt.compare(password, user.password);
-  if (!comparePassword) throw RequestError(401, 'Email or password is wrong');
+  if (!comparePassword) throw RequestError(401, 'Password wrong');
+  const payload = {
+    id: user._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+  res.json({
+    token,
+    user,
+  });
 };
 
 module.exports = login;
