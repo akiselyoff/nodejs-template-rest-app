cc5927b55369730a187e1822e1b467b55cd300be
kesel <a.kiselyoff@gmail.com>
2022-09-06 21:52:43 +0300

    add error to updateAvatar

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,21 +1,22 @@
-const { User } = require('../../models/user');
 const path = require('path');
 const fs = require('fs/promises');
+const { User } = require('../../models/user');
 
 const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
 
 const updateAvatar = async (req, res) => {
-  const { path: tempUpload, originalname } = req.file;
-  const { _id } = req.user;
-  const avatarName = `${_id}_${originalname}`;
   try {
+    const { path: tempUpload, originalname } = req.file;
+    const { _id } = req.user;
+    const avatarName = `${_id}_${originalname}`;
     const resultUpload = path.join(avatarsDir, avatarName);
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = path.join('public', 'avatars', avatarName);
     await User.findByIdAndUpdate(_id, { avatarURL });
     res.json({ avatarURL });
   } catch (error) {
-    await fs.unlink(tempUpload);
+    await fs.unlink(req.file.path);
+    throw error;
   }
 };
 
